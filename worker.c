be130aae8bf65da050186c5779b3ec0c8bfc64c0
jedhf@umsystem.edu <jedhf@umsl.edu>
2026-02-18 12:15:09 -0600

    Worker can now attach and read the clock

--- worker.c
+++ worker.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "shared.h"
 
 int main(int argc, char *argv[]) {
-    printf("Worker test\n");
+    (void) argc;
+    (void) argv;
+
+    int shmid = shmget(SHM_KEY, sizeof(SimClock), 0666);
+    if (shmid == -1) {
+        perror("worker shmget failed");
+        return 1;
+    }
+
+    SimClock *clock = (SimClock *)shmat(shmid, NULL, 0);
+    if (clock == (void *) -1){
+        perror("worker shmat failed");
+        return 1;
+    }
+
+    printf("WORKER starting PID:%d PPID %d\n", getpid(), getppid());
+    printf("WORKER sees clock: %u seconds, %u nanoseconds\n", clock -> seconds, clock -> nanoseconds);
+
+    shmdt(clock);
     return 0;
 }
