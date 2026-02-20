b234fb42b11b77014365990c261784a958213e8e
jedhf@umsystem.edu <jedhf@umsl.edu>
2026-02-19 21:01:53 -0600

    Implement worker lifetime control using shared memory clock

--- worker.c
+++ worker.c
@@ -6,8 +6,14 @@
 #include "shared.h"
 
 int main(int argc, char *argv[]) {
-    (void) argc;
-    (void) argv;
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: worker seconds nanoseconds\n");
+        return 1;
+    }
+
+    unsigned int termSec = atoi(argv[1]);
+    unsigned int termNano = atoi(argv[2]);
 
     int shmid = shmget(SHM_KEY, sizeof(SimClock), 0666);
     if (shmid == -1) {
@@ -15,14 +21,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SimClock *clock = (SimClock *)shmat(shmid, NULL, 0);
-    if (clock == (void *) -1){
+    SimClock *clock = (SimClock *) shmat(shmid, NULL, 0);
+    if (clock == (void *) -1) {
         perror("worker shmat failed");
         return 1;
     }
 
-    printf("WORKER starting PID:%d PPID %d\n", getpid(), getppid());
-    printf("WORKER sees clock: %u seconds, %u nanoseconds\n", clock -> seconds, clock -> nanoseconds);
+    printf("WORKER PID:%d starting at %u:%u\n",
+           getpid(), clock->seconds, clock->nanoseconds);
+
+    while (1) {
+
+        // Check if current simulated time reached termination time
+        if ((clock->seconds > termSec) ||
+            (clock->seconds == termSec && clock->nanoseconds >= termNano)) {
+            break;
+            }
+    }
+
+    printf("WORKER PID:%d terminating at %u:%u\n",
+           getpid(), clock->seconds, clock->nanoseconds);
 
     shmdt(clock);
     return 0;
